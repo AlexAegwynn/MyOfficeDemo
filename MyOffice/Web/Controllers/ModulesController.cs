e6d53681319fe4539cddc7b87e761f705774679c
Alex Aegwynn <1549777384@qq.com>
2018-12-05 18:00:43 +0800

    commit

---
+++ MyOffice/Web/Controllers/ModulesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    public class ModulesController : Controller
+    {
+        //Home 首页
+        public PartialViewResult HomeModule()
+        {
+            return PartialView();
+        }
+
+        //Department management 部门管理
+        public PartialViewResult DepManagement()
+        {
+            return PartialView();
+        }
+
+
+    }
+}
