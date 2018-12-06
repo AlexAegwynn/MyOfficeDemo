dfd8813fb9a7c949afed96523df67c726aa4be14
Alex Aegwynn <1549777384@qq.com>
2018-12-06 17:46:30 +0800

    commit

--- MyOffice/Web/Controllers/ModulesController.cs
+++ MyOffice/Web/Controllers/ModulesController.cs
@@ -20,6 +20,46 @@ namespace Web.Controllers
             return PartialView();
         }
 
+        //Position management 职位管理
+        public PartialViewResult PosMgtModule()
+        {
+            return PartialView();
+        }
+
         //Staff management 员工管理
+        public PartialViewResult StaffMgtModule()
+        {
+            return PartialView();
+        }
+
+        //My schedule 我的日程
+        public PartialViewResult MySchModule()
+        {
+            return PartialView();
+        }
+
+        //Department schedule 部门日程
+        public PartialViewResult DepSchModule()
+        {
+            return PartialView();
+        }
+
+        //MyNote 我的便签
+        public PartialViewResult MyNoteModule()
+        {
+            return PartialView();
+        }
+
+        //Mail 信箱
+        public PartialViewResult MailModule()
+        {
+            return PartialView();
+        }
+
+        //Message management 信息管理
+        public PartialViewResult MsgMgtModule()
+        {
+            return PartialView();
+        }
     }
 }
